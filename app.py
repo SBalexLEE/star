a2552d8fdb2a30bec96bd3e1cd731ef70665944a
SBalexLEE <67940744+SBalexLEE@users.noreply.github.com>
2022-05-04 15:05:37 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/generic")
+def generic():
+    return render_template('generic.html')
+
+@app.route("/elements")
+def elements():
+    return render_template('elements.html')
+
+if __name__ == "__main__":
+    app.run()                                                                                                                                                                
