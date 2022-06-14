a2fb3851b90b492d1d20c2a2c1503c7a41daa68a
Alex Lee <morning0307@gmail.com>
2022-06-14 01:47:11 -0400

    resume download

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
